7a50a8184c44ecf35df482a4382919767e062b6c
Rafael Souza <souza_rafael.97@outlook.com>
2022-06-30 10:56:03 -0300

    Adiciona Requisito 4

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,18 @@
 const data = require('../data/zoo_data');
 
+const { employees } = data;
+
 function isManager(id) {
-  // seu código aqui
+  return employees.some((element) => element.managers.length <= 1 && element.id === id);
 }
 
 function getRelatedEmployees(managerId) {
-  // seu código aqui
+  if (isManager(managerId) === false) {
+    throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
+  }
+  return employees
+    .filter((employee) => employee.managers.includes(managerId))
+    .map((e) => `${e.firstName} ${e.lastName}`);
 }
 
 module.exports = { isManager, getRelatedEmployees };
