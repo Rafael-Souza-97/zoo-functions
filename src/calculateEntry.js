f0a60fe673c387a71e50a28bc98719057844adee
Rafael Souza <souza_rafael.97@outlook.com>
2022-07-01 14:13:28 -0300

    Adiciona Requisito 8

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,21 @@
 const data = require('../data/zoo_data');
 
 function countEntrants(entrants) {
-  // seu código aqui
+  if (!entrants || Object.keys(entrants).length === 0) return 0;
+  const child = entrants.filter((person) => (person.age < 18)).length;
+  const adult = entrants.filter((person) => (person.age >= 18 && person.age < 50)).length;
+  const senior = entrants.filter((person) => (person.age >= 50)).length;
+  const result = { adult, child, senior };
+  return result;
 }
 
 function calculateEntry(entrants) {
-  // seu código aqui
+  if (!entrants || Object.keys(entrants).length === 0) return 0;
+  const childPrice = 20.99 * countEntrants(entrants).child;
+  const seniorPrice = 24.99 * countEntrants(entrants).senior;
+  const adultPrice = 49.99 * countEntrants(entrants).adult;
+
+  return childPrice + seniorPrice + adultPrice;
 }
 
 module.exports = { calculateEntry, countEntrants };
