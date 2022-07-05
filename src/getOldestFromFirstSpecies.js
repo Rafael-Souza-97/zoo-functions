0068a768e7dd31f48c3753229f609496f7d959d0
Rafael Souza <souza_rafael.97@outlook.com>
2022-07-04 21:27:03 -0300

    Adiciona Requisito 9

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -7,8 +7,10 @@ function getOldestFromFirstSpecies(id) {
   const employeeSearch = employees.filter((employee) => id === employee.id);
   const firstAnimal = employeeSearch.map((colaborator) =>
     colaborator.responsibleFor).map((animal) => animal[0]);
-  const animal = species.filter((element) => element.id === firstAnimal[0]).map((element) =>
-    element.residents).reduce((acc, item) => (acc.ge > item.age ? acc : item))
+  const animal = species
+    .filter((element) => element.id === firstAnimal[0])
+    .map((element) => element.residents)
+    .reduce((acc, item) => (acc.ge > item.age ? acc : item))
     .reduce((acc, animalInfo) => (acc.age > animalInfo.age ? acc : animalInfo));
   const results = [animal.name, animal.sex, animal.age];
   return results;
--- src/getSchedule.js
+++ src/getSchedule.js
@@ -1,7 +1,63 @@
 const data = require('../data/zoo_data');
 
+const { species } = data;
+
+const { hours } = data;
+
+const animalDays = (scheduleTarget) =>
+  species.find((animals) => animals.name === scheduleTarget).availability;
+
+function objDays(day) {
+  const search = species.filter((animal) => animal);
+  const animals = search.map((animal) => {
+    if (animal.availability.includes(day)) {
+      return animal.name;
+    }
+    return false;
+  }).filter((item) => item);
+  return animals;
+}
+
+function objMaker() {
+  const schedule = {};
+  Object.entries(hours).forEach((item) => {
+    if (!item[1].open) {
+      schedule[item[0]] = {
+        officeHour: 'CLOSED',
+        exhibition: 'The zoo will be closed!',
+      };
+    } else {
+      schedule[item[0]] = {
+        officeHour: `Open from ${item[1].open}am until ${item[1].close}pm`,
+        exhibition: objDays(item[0]),
+      };
+    }
+  });
+  return schedule;
+}
+
+function days(scheduleTarget) {
+  const schedule = {};
+  Object.entries(hours).forEach((item) => {
+    if (!item[1].open && item[0] === scheduleTarget) {
+      schedule[item[0]] = {
+        officeHour: 'CLOSED',
+        exhibition: 'The zoo will be closed!',
+      };
+    } else if (item[0] === scheduleTarget) {
+      schedule[item[0]] = {
+        officeHour: `Open from ${item[1].open}am until ${item[1].close}pm`,
+        exhibition: objDays(item[0]),
+      };
+    }
+  });
+  return schedule;
+}
+
 function getSchedule(scheduleTarget) {
-  // seu código aqui
+  if (species.some((animal) => animal.name === scheduleTarget)) return animalDays(scheduleTarget);
+  if (!Object.keys(hours).includes(scheduleTarget)) return objMaker();
+  if (Object.keys(hours).includes(scheduleTarget)) return days(scheduleTarget);
 }
 
 module.exports = getSchedule;
